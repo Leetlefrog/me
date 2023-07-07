ae74d96f4fd24fce44a71e1abfef6c79a21e2e1d
Leetlefrog <rana@codessi.net>
2023-07-07 02:55:39 +0000

    This is where I tried to commit without messaging

--- set2/exercise2.py
+++ set2/exercise2.py
@@ -11,7 +11,7 @@ file from top to bottom.
 import string
 
 # E.g. det isn't a reserved word, it should be def
-det getLetter(index):
+def getLetter(index):
     the_alphabet = string.ascii_lowercase + " "
     return the_alphabet(index] # <-- this should be using [] to index into a list
 
